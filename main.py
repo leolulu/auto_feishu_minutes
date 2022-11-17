710938a099ae2a3a9ac9817d9286c084aa3a77a7
surface@pro.3 <348699103@qq.com>
2022-11-17 23:34:26 +0800

    增加没有字幕时的提前跳出判断

--- main.py
+++ main.py
@@ -28,6 +28,8 @@ class PostUploader:
 
     def upload_laucher(self, switch_between_post_uploads):
         self.level_upload(switch_between_post_uploads)
+        if self.all_finish:
+            return
         if self.app.sub_downloaded or (self.current_level == self.level_target):
             self.current_level += 1
             self.app = None  # type: ignore
