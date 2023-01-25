c4fd44c6e07e3561b1973c6903f80fa0f5a75181
surface@pro.3 <348699103@qq.com>
2023-01-25 16:18:12 +0800

    限制二次切割时长单句的长度

--- utils/read_srt.py
+++ utils/read_srt.py
@@ -1,7 +1,23 @@
+import datetime
+import re
+
 import srt
 
 
-def read_srt(srt_file_path: str):
+def _compute_end_time(start_delta: datetime.timedelta, end_delta: datetime.timedelta, content: str, max_delta_second):
+    if (
+        isinstance(max_delta_second, int)
+        and ((end_delta.seconds-start_delta.seconds) > max_delta_second)
+        and re.search(r".*oh.*", content.lower())
+    ):
+        return srt.timedelta_to_srt_timestamp(
+            start_delta + datetime.timedelta(seconds=max_delta_second)
+        )
+    else:
+        return srt.timedelta_to_srt_timestamp(end_delta)
+
+
+def read_srt(srt_file_path: str, max_delta_second=None):
     with open(srt_file_path, 'r', encoding='utf-8') as f:
         data = f.read()
         srt_datas = srt.parse(data)
@@ -12,7 +28,7 @@ def read_srt(srt_file_path: str):
         content = srt_data.content
         start_time = srt.timedelta_to_srt_timestamp(srt_data.start)
         start_time = start_time.replace(",", ".")
-        end_time = srt.timedelta_to_srt_timestamp(srt_data.end)
+        end_time = _compute_end_time(srt_data.start, srt_data.end, content, max_delta_second)
         end_time = end_time.replace(",", ".")
         result.append([start_time, end_time, content])
     return result
@@ -27,4 +43,4 @@ def if_srt_empty(srt_file_path: str):
 
 
 if __name__ == '__main__':
-    print(read_srt(r"C:\fuck\新建文件夹\自慰初体验（直播） (1).srt"))
+    print(read_srt(r"C:\Users\pro3\Downloads\16356.srt", max_delta_second=3))
